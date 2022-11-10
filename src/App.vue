3508fcdc3ad490d3d356253374ed2e42682d6030
eliasyoung <warlockD@126.com>
2022-11-10 17:51:11 +0800

    add navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <RouterView />
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <site-navigation />
+    <router-view />
   </div>
 </template>
 
 <script setup>
-import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
-<style lang="less" scoped></style>
