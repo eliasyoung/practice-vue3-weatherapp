d4a330712663f4c48e3b9578c16aebf3a71b2605
eliasyoung <warlockD@126.com>
2022-11-10 21:40:07 +0800

    finish BaseModal Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
-    <site-navigation />
-    <router-view />
+    <SiteNavigation />
+    <RouterView />
   </div>
 </template>
 
 <script setup>
 import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
-
