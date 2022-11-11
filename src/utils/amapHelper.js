03f3c9a53d0244fe201f3d5adf702963f03062c3
eliasyoung <warlockD@126.com>
2022-11-11 23:17:47 +0800

    add amap and finish homeview component

---
+++ src/utils/amapHelper.js
@@ -0,0 +1,41 @@
+import AMapLoader from "@amap/amap-jsapi-loader";
+
+window._AMapSecurityConfig = {
+  securityJsCode: "",
+};
+
+const initAmap = async () => {
+  return await AMapLoader.load({
+    key: "", // 申请好的Web端开发者Key，首次调用 load 时必填
+    version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+    plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+  });
+};
+
+const amap = await initAmap();
+
+export const initGeocoder = (city = "全国") => {
+  let geocoder;
+  amap.plugin("AMap.Geocoder", function () {
+    geocoder = new amap.Geocoder({
+      // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+      city,
+      batch: true,
+    });
+  });
+  return geocoder;
+};
+
+export const initPlaceSearch = (city = "全国") => {
+  //   const placeSearch = new amap.PlaceSearch({
+  //     // city 指定搜索所在城市，支持传入格式有：城市名、citycode和adcode
+  //     city,
+  //   });
+  let placeSearch;
+  amap.plugin("AMap.PlaceSearch", function () {
+    placeSearch = new amap.PlaceSearch({
+      city,
+    });
+  });
+  return placeSearch;
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,62 @@
-<script setup>
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+        v-model.trim="searchQuery"
+        @keyup.enter="getSearchResult"
+        @focus="clearSearchResult"
+      />
+      <ul
+        v-if="amapSearchResult"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <p v-if="amapSearchResult.length === 0">没有找到合适的结果</p>
+        <li
+          class="py-2 cursor-pointer"
+          v-for="result in amapSearchResult"
+          :key="result.id"
+        >
+          {{ result.name }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { initPlaceSearch } from "@/utils/amapHelper.js";
+
+const placeSearch = initPlaceSearch();
+
+const searchQuery = ref("");
+const amapSearchResult = ref(null);
+// const queryTimer = ref(null);
+
+const getSearchResult = () => {
+  // clearTimeout(queryTimer.value);
+  // queryTimer.value = setTimeout(() => {
+  //   if(searchQuery.value != '') {
+  //   }
+  // }, 3000);
+  if (searchQuery.value != "") {
+    placeSearch.search(searchQuery.value, function (status, result) {
+      // 查询成功时，result即对应匹配的POI信息
+      if (status === "complete" && result.info === "OK") {
+        amapSearchResult.value = result.poiList.pois;
+        return;
+      }
+      if (status === "no_data") {
+      }
+      amapSearchResult.value = [];
+    });
+  }
+};
+
+const clearSearchResult = () => {
+  amapSearchResult.value = null;
+};
+</script>
